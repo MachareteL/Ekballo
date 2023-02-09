6210c8ffd24e19059aa37c0f3a5061d7db5a86af
MachareteL <116664977+MachareteL@users.noreply.github.com>
2023-02-09 14:45:05 -0300

    Do nothing index

--- src/pages/matricula/index.jsx
+++ src/pages/matricula/index.jsx
@@ -74,9 +74,9 @@ export default function cadastro(session) {
         event.target.endereco.value = ""
         event.target.idade.value = ""
         event.target.curso.value = ""
-        event.target.responsavel.value = ""
-        event.target.documentoPai.value = "",
-        event.target.documentoAluno.value = "",
+        anos <= 18 ? event.target.responsavel.value = "" : 
+        anos <= 18 ? event.target.documentoPai.value = "" :
+        event.target.documentoAluno.value = ""
         response.ok? swal(result.result) : swal(result.error)
     }
 
