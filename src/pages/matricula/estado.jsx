a513be67848605bcf67909e57cf8859a184127b1
MachareteL <macharetelucas@gmail.com>
2023-02-10 09:02:01 -0300

    working on estado route

--- src/pages/matricula/estado.jsx
+++ src/pages/matricula/estado.jsx
@@ -10,10 +10,18 @@ export default function estado(et){
         <>
         <h1 className="text-xl font-bold">Matriculas</h1>
             {matriculas.map((matricula)=>(
-                <ul key={matricula._id}>
-                    <li>{matricula.nome}</li>
-                    <li>{matricula.telefone}</li>
-                </ul>
+                <div class="flex justify-center">
+                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                  <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt="" />
+                  <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">{matricula.curso}</h5>
+                    <p class="text-gray-700 text-base mb-4">
+                      Você está matriculado no curso de {matricula.curso}
+                    </p>
+                    <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                  </div>
+                </div>
+              </div>
             ))}
         </>
     );
