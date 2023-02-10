60361a134aa8490f6c9a452aa92e1e4d872720cc
MachareteL <macharetelucas@gmail.com>
2023-02-10 08:37:13 -0300

    input correction

--- src/pages/matricula/index.jsx
+++ src/pages/matricula/index.jsx
@@ -96,7 +96,7 @@ export default function cadastro(session) {
                     </div>
                     <div className="flex col-span-2">
                         <label htmlFor="telefone" className="text-white mx-4">Telefone: </label>
-                        <input required type="tel" name="telefone" id="telefone" placeholder="* (DDD)9 XXXX-XXXX" className="caret-white text-white bg-slate-900 rounded p-2 h-7 w-full mr-4" />
+                        <input required type="number" name="telefone" id="telefone" placeholder="* (DDD)9 XXXX-XXXX" className="caret-white text-white bg-slate-900 rounded p-2 h-7 w-full mr-4" />
                     </div>
                     <div className="col-span-1 grid sm:flex">
                         <label htmlFor="idade" className="text-white mx-4">Data de Nascimento: </label>
@@ -130,19 +130,19 @@ export default function cadastro(session) {
                         <>
                             <div className="flex col-span-2 ">
                                 <label htmlFor="responsavel" className="text-white mx-4">Responsável: </label>
-                                <input type="text" name="responsavel" id="responsavel" placeholder="* Nome do Pai ou Mâe" className="caret-white text-white bg-slate-900 rounded p-2 h-7 w-full mr-4" />
+                                <input required type="text" name="responsavel" id="responsavel" placeholder="* Nome do Pai ou Mâe" className="caret-white text-white bg-slate-900 rounded p-2 h-7 w-full mr-4" />
                             </div>
 
                             <div className="flex col-span-2">
                                 <label htmlFor="documentoPai" className="text-white mx-4">Documento do Responsável:</label>
-                                <input type="text" name="documentoPai" id="documentoPai" placeholder="CPF ou RG" className="caret-white text-white bg-slate-900 p-2 h-7 w-full mr-4 rounded" />
+                                <input required type="text" name="documentoPai" id="documentoPai" placeholder="CPF ou RG" className="caret-white text-white bg-slate-900 p-2 h-7 w-full mr-4 rounded" />
                             </div>
                         </>
                         : <></>}
 
                     <div className="flex col-span-2 items-baseline justify-center">
                         <input required type="checkbox" name="termos" id="termos" />
-                        <label htmlFor="termos" className="text-center text-white w-11/12"><a role="button" className="underline" onClick={()=>{swal({title: "TERMOS DE USO E PRIVACIDADE",text:"O Projeto Social Ekballo é uma instituição cristã e sem fins lucrativos, todos os dados aqui coletados serão utilizados somente para os fins de cadastro e registro no banco de dados de alunos da Escola.\n\nEkballo se compromete em não divulgar, vender e usar os dados aqui fornecidos para nenhuma outra atividade senão contatar alunos e responsáveis em caso de falta, mudança de horário ou cancelamento de eventuais aulas e/ou eventos.\n\n Ao aceitar os termos confirmo que autorizo a posse dos meus dados e informações pela instituição Ekballo assim como autorizo que entrem em contato caso necessário pelo telefone e/ou email aqui fornecidos.", button: "Confirmo"})}}> Li e Concordo</a> com os termos de uso, ciente de todos os termos de politica e privacidade.</label>
+                        <label htmlFor="termos" className="text-center text-white w-11/12"><a role="button" className="underline" onClick={()=>{swal({title: "TERMOS DE USO E PRIVACIDADE",text:"O Projeto Social Ekballo é uma instituição cristã e sem fins lucrativos, todos os dados aqui coletados serão utilizados somente para os fins de cadastro e registro no banco de dados de alunos da Escola.\n\nEkballo se compromete em não divulgar, vender e usar os dados aqui fornecidos para nenhuma outra atividade senão contatar alunos e responsáveis em caso de falta, mudança de horário ou cancelamento de eventuais aulas e/ou eventos.\n\n Ao aceitar os termos confirmo que autorizo a posse dos meus dados e informações pela instituição Ekballo assim como autorizo que entrem em contato caso necessário pelo telefone e/ou email aqui fornecidos.", button: "Aceito"})}}> Li e Concordo</a> com os termos de uso, ciente de todos os termos de politica e privacidade.</label>
                     </div>
                     <div className="col-span-2 flex justify-end">
                         <button type="submit" className="bg-gray-600 p-2 rounded-lg text-white mr-4 hover:bg-blue-700 active:bg-slate-700">Matricular</button>
