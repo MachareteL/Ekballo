aa19511dcf89f2637fe5e44e9b670fa8294fb408
MachareteL <macharetelucas@gmail.com>
2023-02-10 09:35:22 -0300

    working on estado route

--- src/pages/matricula/estado.jsx
+++ src/pages/matricula/estado.jsx
@@ -9,8 +9,8 @@ export default function estado(et){
     return(
         <>
         <h1 className="text-xl font-bold">Matriculas</h1>
+        <div class="flex justify-center">
             {matriculas.map((matricula)=>(
-                <div class="flex justify-center">
                 <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
                   <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt="" />
                   <div class="p-6 flex flex-col justify-start">
@@ -21,8 +21,8 @@ export default function estado(et){
                     <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
                   </div>
                 </div>
-              </div>
             ))}
+            </div>
         </>
     );
 }
